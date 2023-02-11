967783df4e82c4eb8da211be981b05d64ce90067
gs7joao <gsjoaopedro7@gmail.com>
2023-02-11 14:31:22 -0300

    Estruturando projeto Alura Organograma

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O Banner principal da página do Organograma"/>
+        </header>
+    )
+}
+
+export default Banner
